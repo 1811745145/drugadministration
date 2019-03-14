9f06e6f5beec6ceee90ba00b35921439ef0fcea1
zhangzb <782479115@qq.com>
2019-03-14 10:27:12 +0800

    代理信息后台增删改查  企业资质后套新增

--- products_api/src/main/java/com/jk/service/ProductService.java
+++ products_api/src/main/java/com/jk/service/ProductService.java
@@ -1,8 +1,9 @@
 package com.jk.service;
 
+import com.jk.pojo.CertificateBean;
+import com.jk.pojo.EnterpriseBean;
 import com.jk.pojo.ProductsBean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RequestMapping("products")
@@ -11,7 +12,47 @@ public interface ProductService {
          * 查询全部代理信息
          * @return
          */
-        @GetMapping("findProductsAll")
+        @RequestMapping("findProductsAll")
         List<ProductsBean> findProductsAll();
+
+
+        /**
+         * 新增代理需求
+         */
+        @RequestMapping(value = "saveProducts",method = RequestMethod.POST)
+        @ResponseBody
+        void saveProducts(@RequestBody ProductsBean productsBean);
+        /**
+         * 根据状态查询代理信息
+         * @return
+         */
+        @RequestMapping("findProductsStatusList")
+        List<ProductsBean> findProductsStatusList(@RequestParam("productsStatus")Integer productsStatus);
+        /**
+         * 删除代理信息
+         */
+        @RequestMapping("productsId")
+        void delProduct(@RequestParam("productsId")Integer productsId);
+
+        /**
+         * 根据id查询代理信息 （回显）
+         * @param productsId
+         * @return
+         */
+        @RequestMapping("findProductsById")
+        ProductsBean findProductsById(@RequestParam("productsId")Integer productsId);
+
+
+        /**
+         * 新增企业资质
+         */
+        @RequestMapping("saveEnterprise")
+        void saveEnterprise( @RequestBody EnterpriseBean enterpriseBean);
+
+        /**
+         * 新增企业营业执照
+         */
+        @RequestMapping("saveCertificate")
+        void saveCertificate(@RequestBody CertificateBean certificateBean);
 
 }
