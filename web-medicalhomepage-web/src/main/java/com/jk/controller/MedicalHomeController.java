afda8adb43dc068896a670147acc430e5341ce58
Lfjnb <910419990@qq.com>
2019-03-19 20:45:43 +0800

    增加子项目 药品快速采购  药品信息查询 供应商查询  接口

--- web-medicalhomepage-web/src/main/java/com/jk/controller/MedicalHomeController.java
+++ web-medicalhomepage-web/src/main/java/com/jk/controller/MedicalHomeController.java
@@ -2,8 +2,14 @@ package com.jk.controller;
 
 import com.jk.service.MedicalHomeServiceFeign;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("MedicalHomeController")
@@ -12,14 +18,24 @@ public class MedicalHomeController {
     @Autowired
     private MedicalHomeServiceFeign  medicalHomeServiceFeign;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @RequestMapping("junmpMedicalHome")
     public   String  junmpMedicalHome(){
        return "home";
     }
     @RequestMapping("junmpclassify")
-    public   String  junmpclassify(){
+    public   String  junmpclassify(String name, Model  Model){
+        Model.addAttribute("name",name);
         return "classify";
+    }
+
+    /*添加购物车*/
+    @ResponseBody
+    @RequestMapping("addshping")
+    public   String  addshping(HttpServletRequest request){
+        return "1";
     }
 
 }
