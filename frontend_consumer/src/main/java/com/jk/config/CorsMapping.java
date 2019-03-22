b82d4fa4e00cc0132334670f433891e4cd60694b
xufei <1178911715@qq.com>
2019-03-22 17:11:40 +0800

    修改了前端消费者医药精选接口和前台

---
+++ frontend_consumer/src/main/java/com/jk/config/CorsMapping.java
@@ -0,0 +1,123 @@
+package com.jk.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.*;
+
+import java.util.List;
+
+@Configuration
+public class CorsMapping implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer pathMatchConfigurer) {
+
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer contentNegotiationConfigurer) {
+
+    }
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer asyncSupportConfigurer) {
+
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer defaultServletHandlerConfigurer) {
+
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry formatterRegistry) {
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry) {
+
+    }
+
+    @Override
+    /**
+     * 重新跨域支持方法
+     * CorsRegistry  开启跨域注册
+     */
+    public void addCorsMappings(CorsRegistry registry) {
+        //addMapping 添加可跨域的请求地址
+        registry.addMapping("/**")
+                //设置跨域 域名权限 规定由某一个指定的域名+端口能访问跨域项目localhost:80
+                .allowedOrigins("*")
+                //是否开启cookie跨域
+                .allowCredentials(false)
+                //规定能够跨域访问的方法类型
+                .allowedMethods("GET","POST","DELETE","PUT","OPTIONS","HEAD")
+                //添加验证头信息  token
+                //.allowedHeaders()
+                //预检请求存活时间 在此期间不再次发送预检请求
+                .maxAge(3600);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
+
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry viewResolverRegistry) {
+
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> list) {
+
+    }
+
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> list) {
+
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> list) {
+
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> list) {
+
+    }
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> list) {
+
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> list) {
+
+    }
+
+    @Override
+    public Validator getValidator() {
+        return null;
+    }
+
+    @Override
+    public MessageCodesResolver getMessageCodesResolver() {
+        return null;
+    }
+}
